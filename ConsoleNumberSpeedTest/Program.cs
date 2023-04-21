0dcd039f6738189bbca576763104f5357e8017a4
omeralmali1 <omeralmali1@gmail.com>
2023-04-21 20:39:36 +0300

    Add project files.

---
+++ ConsoleNumberSpeedTest/Program.cs
@@ -0,0 +1,12 @@
+﻿DateTime firstTime, lastTime;
+firstTime = DateTime.Now;
+int allCount = 0;
+Random rnd = new Random();
+for (int i = 0; i < 1000; i++)
+{
+    allCount += rnd.Next(1, 1001);
+}
+lastTime = DateTime.Now;
+TimeSpan speed = lastTime - firstTime;
+Console.WriteLine(speed.TotalMilliseconds);
+Console.ReadLine();
