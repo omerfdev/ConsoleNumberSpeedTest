b7bbff4741e3cf509393bca72ec90239314936af
omerfdev <omerfdev@gmail.com>
2023-06-18 21:46:03 +0300

    Commit

--- ConsoleNumberSpeedTest/Program.cs
+++ ConsoleNumberSpeedTest/Program.cs
@@ -1,12 +1,30 @@
-﻿DateTime firstTime, lastTime;
-firstTime = DateTime.Now;
-int allCount = 0;
-Random rnd = new Random();
-for (int i = 0; i < 1000; i++)
+﻿namespace ConsoleNumberSpeedTest
 {
-    allCount += rnd.Next(1, 1001);
+   
+   
+    public class Program
+    {
+        public delegate void SpeedTestDelegate();
+        public static SpeedTestDelegate speedTestdelegate = speedTest;
+        public static void speedTest()
+        {
+            DateTime firstTime, lastTime;
+            firstTime = DateTime.Now;
+            int allCount = 0;
+            Random rnd = new Random();
+            for (int i = 0; i < 1000; i++)
+            {
+                allCount += rnd.Next(1, 1001);
+            }
+            lastTime = DateTime.Now;
+            TimeSpan speed = lastTime - firstTime;
+            Console.WriteLine(speed.TotalMilliseconds);
+            Console.ReadLine();
+        }
+        public static void Main(string[] args)
+        {
+            speedTestdelegate();
+        }
+    }
 }
-lastTime = DateTime.Now;
-TimeSpan speed = lastTime - firstTime;
-Console.WriteLine(speed.TotalMilliseconds);
-Console.ReadLine();
+
